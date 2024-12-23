faeb0ad38321dce950aaf9ec7d5da86624f0e242
Akash Kriplani <aakash23kriplani@gmail.com>
2024-12-23 21:15:53 +0530

    002: Implement CRUD using signal based approach

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,12 +1,13 @@
-import {Component, effect, inject, signal} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
-import {Course} from "../models/course.model";
-import {EditCourseDialogData} from "./edit-course-dialog.data.model";
-import {CoursesService} from "../services/courses.service";
-import {LoadingIndicatorComponent} from "../loading/loading.component";
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
-import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
-import {CourseCategory} from "../models/course-category.model";
+import { Component, effect, inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material/dialog";
+import { Course } from "../models/course.model";
+import { EditCourseDialogData } from "./edit-course-dialog.data.model";
+import { CoursesService } from "../services/courses.service";
+import { LoadingIndicatorComponent } from "../loading/loading.component";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CourseCategoryComboboxComponent } from "../course-category-combobox/course-category-combobox.component";
+import { CourseCategory } from "../models/course-category.model";
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'edit-course-dialog',
@@ -20,6 +21,68 @@ import {CourseCategory} from "../models/course-category.model";
   styleUrl: './edit-course-dialog.component.scss'
 })
 export class EditCourseDialogComponent {
+  courseService = inject(CoursesService);
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA);
+  dialogRef = inject(MatDialogRef);
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    title: [''],
+    longDescription: [''],
+    category: [''],
+    iconUrl: [''],
+  });
 
+  constructor() {
+    this.form.patchValue({
+      title: this.data.course?.title,
+      longDescription: this.data.course?.longDescription,
+      category: this.data.course?.category,
+      iconUrl: this.data.course?.iconUrl,
+    });
+  }
+
+  createCourse = async (courseProps: Partial<Course>) => {
+    try {
+      const newCourse = await this.courseService.createCourse(courseProps);
+      this.dialogRef.close(newCourse);
+    } catch (err) {
+      console.error('Error creating course', err);
+      alert('Failed to create course.');
+    }
+  }
+
+  onClose = () => this.dialogRef.close();
+
+  onSave = async () => {
+    const courseProps = this.form.value as Partial<Course>;
+
+    if (this.data?.mode === 'update') {
+      await this.saveCourse(this.data.course!.id, courseProps);
+    } else if (this.data?.mode === 'create') {
+      await this.createCourse(courseProps);
+    }
+  }
+
+  saveCourse = async (courseId: string, courseProps: Partial<Course>) => {
+    try {
+      const updatedCourse = await this.courseService.saveCourse(courseId, courseProps);
+      this.dialogRef.close(updatedCourse);
+    } catch (error) {
+      console.error('Error saving course', error);
+      alert('Failed to save course.');
+    }
+  }
+}
+
+export const openEditCourseDialog = async (dialog: MatDialog, data: EditCourseDialogData) => {
+  const config = new MatDialogConfig();
+  config.autoFocus = true;
+  config.disableClose = true;
+  config.data = data;
+  config.width = '400px';
+
+  const close$ = dialog.open(EditCourseDialogComponent, config).afterClosed();
+
+  return firstValueFrom(close$);
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,84 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
+import { CoursesService } from "../services/courses.service";
+import { Course, sortCoursesBySeqNo } from "../models/course.model";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { CoursesCardListComponent } from "../courses-card-list/courses-card-list.component";
+import { MatDialog } from "@angular/material/dialog";
+import { MessagesService } from "../messages/messages.service";
+import { catchError, from, throwError } from "rxjs";
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from "@angular/core/rxjs-interop";
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
+import { openEditCourseDialog } from '../edit-course-dialog/edit-course-dialog.component';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [
+    MatTabGroup,
+    MatTab,
+    CoursesCardListComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  advancedCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter(({ category }) => category === 'ADVANCED')
+  });
 
+  beginnerCourses = computed(() => {
+    const courses = this.#courses();
+    return courses.filter(({ category }) => category === 'BEGINNER')
+  });
+
+  coursesService = inject(CoursesService);
+  dialog = inject(MatDialog);
+
+  #courses = signal<Course[]>([]);
+
+  constructor() {
+    this.loadCourses().then(() => console.log(`All courses loaded: `, this.#courses()));
+  }
+
+  onAddCourse = async () => {
+    const newCourse = await openEditCourseDialog(this.dialog, {
+      mode: 'create',
+      title: 'Create new course',
+    });
+
+    if (!newCourse) {
+      return;
+    }
+
+    const newCourses = [...this.#courses(), newCourse];
+    this.#courses.set(newCourses);
+  }
+
+  onCourseDeleted = async (courseId: string) => {
+    try {
+      await this.coursesService.deleteCourse(courseId);
+      const courses = this.#courses();
+      const newCourses = courses.filter(({ id }) => id !== courseId);
+      this.#courses.set(newCourses);
+    } catch (err) {
+      alert('Error deleting course!');
+      console.error(err);
+    }
+  }
+
+  onCourseUpdated = (updatedCourse: Course) => {
+    const courses = this.#courses();
+    const newCourses = courses.map(course => course.id === updatedCourse.id ? updatedCourse : course);
+    this.#courses.set(newCourses);
+  }
+
+  loadCourses = async () => {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.#courses.set(courses.sort(sortCoursesBySeqNo));
+    } catch (err) {
+      alert('Error loading courses!');
+      console.error(err);
+    }
+  }
 }
